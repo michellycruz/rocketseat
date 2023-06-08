03302a871dbb42840921653e964d4c7e2c140d89
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-08 12:53:47 -0300

    Create desafios.js

---
+++ javascript/higher_order_functions/desafios.js
@@ -0,0 +1,14 @@
+/*
+Adicione o ano de nascimento das crianças para cada elemento do array
+*/
+
+
+
+/**
+ * Crie um array somente com os domínios da lista de email
+ */
+
+
+/**
+ * Encontre o maior preço dos produtos abaixo de 100 reais
+ */
