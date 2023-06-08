c8a9091792d1097965ae5150e70c12ee12a8eb27
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-08 10:21:32 -0300

    Update filter.js

--- javascript/higher_order_functions/funcs/filter.js
+++ javascript/higher_order_functions/funcs/filter.js
@@ -1,2 +1,13 @@
 //FILTER
-const { childrenAge } =  require('./')
+const { childrenAge } =  require('../data')
+
+
+//1. Crianças entre 2 - 11 anos
+
+function childrenAge2to11(element, index, array){
+    if(element >= 2 && element <= 11) return true;
+
+    return false;
+}
+
+childrenAge.filter(childrenAge2to11)
