88e548b9b30421b63d88fc4422bfb854b965ef77
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-08 12:50:55 -0300

    Update sort.js

--- javascript/higher_order_functions/funcs/sort.js
+++ javascript/higher_order_functions/funcs/sort.js
@@ -2,9 +2,16 @@
 //ordenar elementos
 //ordenação segue a regra do unicode ascii
 
-const {shopCart} = require("../data")
+const {shopCart, passengers} = require("../data")
 //1. Organize o carrinho do maior número de produtos para o menor
 const orderByTotalProducts = shopCart.sort((a, b) => b.quantity - a.quantity);
 
-console.table(orderByTotalProducts)
+//console.table(orderByTotalProducts)
 //2. organize os passageiros por nome.
+const orderByPassengerName = passengers.sort((pa, pb) => {
+    if(pa.name.toLowerCase() < pb.name.toLowerCase()) return -1;
+    if(pa.name.toLowerCase() > pb.name.toLowerCase()) return 1;
+    return 0;
+});
+
+console.table(orderByPassengerName);
