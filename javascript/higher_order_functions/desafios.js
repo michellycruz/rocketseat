037d4d87851b801beb1820b98142cac56939a27c
Mich <125368412+michellycruz@users.noreply.github.com>
2023-06-08 18:59:15 -0300

    Update desafios.js

--- javascript/higher_order_functions/desafios.js
+++ javascript/higher_order_functions/desafios.js
@@ -1,18 +1,29 @@
+const {childrenAge, passengers} = require("./data");
+const {mailList} = require("./data");
 
-const {childrenAge} = require("./data");
+
 
 /*
 Adicione o ano de nascimento das crianças para cada elemento do array
 */
+const children = childrenAge.map(child => {
+    return{
+        Age: child,
+        birthYear: 2023 - child
+    }
+})
 
-
+console.table(children)
 
 /**
  * Crie um array somente com os domínios da lista de email
  */
+const domains = mailList.map(mail =>{
+    const mailArray = mail.split("@")
+    return mailArray[1]
+})
 
-
-
+console.table(domains)
 /**
  * Encontre o maior preço dos produtos abaixo de 100 reais
  */
